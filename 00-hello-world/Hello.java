16d3558b34ad596ebe8dcc46ff1be327b8d92730
Shahzaib <shahzaib@AC1MWK0MVJC9H.local>
2021-09-27 08:18:19 -0400

    First weeks work in apcsa

---
+++ 00-hello-world/Hello.java
@@ -0,0 +1,5 @@
+public class Hello{//class
+    public static void main(String[] args) {//main method
+      System.out.println("Hello World!");//outputs Hello World
+    }//closes the main method
+}//closes the class\ j
---
+++ 01-perfect-day/PerfectDay.java
@@ -0,0 +1,32 @@
+public class PerfectDay{
+  public static void main(String[] args){
+    System.out.println("My perfect day:");
+    morning();
+    noon();
+    night();
+  }//ends main method
+
+public static void morning(){
+  System.out.println("\nOn the perfect day I would sleep until 12.");
+  System.out.println("I would eat breakfast and go on a half hour jog.");
+}
+
+public static void noon(){
+  System.out.print("\nWhen I get home I would get on my pc, do my work, ");
+  System.out.println("play games with friends and then eat lunch.");
+}
+
+public static void night(){
+  System.out.println("\nWhen its time for dinner I would eat pasta.");
+  System.out.println("I would then watch tv shows I enjoy and then sleep late at about 12 or 1.\n");
+}
+
+//On the perfect day I will sleep until 12.
+//When I wake up I would eat breakfast
+//I would go on a half hour jog
+/*
+When I get home I would get on my pc, do my work, play games with friends
+I would then eat pasta for dinner and watch tv shows I enjoyed
+I would go to sleep late at about 12 or 1
+*/
+}//ends class
