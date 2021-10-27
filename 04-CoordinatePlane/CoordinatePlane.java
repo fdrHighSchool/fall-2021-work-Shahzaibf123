741b731119c57bf1cbb0e0c454ca67fb4492fea1
Shahzaib Fareed <shahzaibf@nycstudents.net>
2021-10-27 08:45:23 -0400

    CoordinatePlane started

---
+++ 04-CoordinatePlane/CoordinatePlane.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class CoordinatePlane{
+  public static void main(String[] args){
+    int x;
+    int y;
+    int x2;
+    int y2;
+    Scanner s = new Scanner(System.in);
+
+    System.out.println("Give me your first point as (x, y)");
+    String point1 = s.nextLine();
+
+    System.out.println("Give me a second point as (x,y)");
+    String point2 = s.nextLine();
+
+    
+
+  }
+
+}
