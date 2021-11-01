1288ec2c47b508354b1212a04f5af989adc16545
Shahzaib Fareed <shahzaibf@nycstudents.net>
2021-11-01 08:36:41 -0400

    comments made and code finished

--- 04-CoordinatePlane/CoordinatePlane.java
+++ 04-CoordinatePlane/CoordinatePlane.java
@@ -24,16 +24,18 @@ public class CoordinatePlane{
     y = Integer.parseInt(point1.substring(point1.indexOf(",")+2,point1.length()-1));
     x2 = Integer.parseInt(point2.substring(1,point2.indexOf(",")));
     y2 = Integer.parseInt(point2.substring(point2.indexOf(",")+2,point2.length()-1));
+    //gets CrowDistance and TaxicabDistance
     distance = CrowDistance(x, x2, y, y2);
     System.out.println("The distance between the two points is " + distance + ".");
     tdistance = TaxicabDistance(x, x2, y, y2);
     System.out.println("The distance between the two points, in taxicab, is " + tdistance + ".");
-
   }
   public static double CrowDistance(int x1, int x2, int y1, int y2){
+    //distance formula sqrt[(y2-y1)^2+(x2-x1)^2]
     return Math.sqrt((Math.pow((y2-y1),2)+(Math.pow((x2-x1),2))));
   }
   public static int TaxicabDistance(int x1, int x2, int y1, int y2){
+    //taxi cab distance |y2-y1|+|x2-x1|
     return Math.abs(y2-y1)+ Math.abs(x2-x1);
   }
 }
