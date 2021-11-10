62cb92c39be1c6e2e95f26aadbb6158fc7bad927
Shahzaib Fareed <shahzaibf@nycstudents.net>
2021-11-10 08:45:23 -0500

    first operand almost parsed

--- 05 - FracCalc/FracCalc.java
+++ 05 - FracCalc/FracCalc.java
@@ -19,10 +19,35 @@ public static String produceAnswer(String input){//1_5/2 + 7/2
 
   String opera1 = input.substring(0, input.indexOf(" "));
   String opera2 = input.substring(input.lastIndexOf(" ") + 1, input.length());
+  int denomright;
+  int denomleft;
+  int wholeleft;
+  int wholeright;
+  int numleft;
+  int numright;
 
-  
-  if (input.contains("+")){
+  if (!(opera1.contains("/"))){
+    denomleft = 1;
+    wholeleft = opera1.substring(0,1);
+    numerator = wholeleft
+  }
+  else{
+    denomleft  = opera1.substring(opera1.indexOf("/") + 1, opera1.length());
+    if (opera1.contains("_")){
+      
+      numleft = opera1.substring(opera1.indexOf("_"),opera1.indexOf("/"));
+    }
+  }
 
+  if (opera2.contains("/")){
+     denomright = opera2.substring(opera2.indexOf("/")+1,opera2.length()); }
+  else{
+     denomright = 1;
   }
-  }
+
+
 }
+  public static int LCM(int x, int y){
+    return 0;
+  }
+  }
