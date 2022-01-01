649f111cd640e931fca38445abe6ab345962837b
Shahzaibf123 <shahzaibf@nycstudents.net>
2022-01-01 13:06:09 -0500

    finished median

--- 06 - Central measures/median/median.java
+++ 06 - Central measures/median/median.java
@@ -2,35 +2,34 @@ import java.util.*;
 public class median{
   public static void main(String[] args){
     int[] arr = {2,4,1,8,5,6};
-    System.out.println(sort(arr));
+    System.out.println(median(sort(arr)));
     //System.out.println(median(arr));
   }
-  public static String sort(int[] arr){
-    int[] sorted = new int[arr.length];
+  public static int[] sort(int[] arr){
     int min = 9999999;
     int temp = 0;//selection sort
-  //  for (int j = 0; j < arr.length; j++){
-      for (int i = 0; i < arr.length;i++){
-        
+    for (int j = 0; j < arr.length - 1; j++){
+      temp = j;
+      for (int i = j + 1; i < arr.length;i++){
+        if(arr[i] < arr[temp]){
+          temp = i;
+        }
       }
-  //  }
+      min = arr[temp];
+      arr[temp] = arr[j];
+      arr[j] = min;
+    }
+    return arr;
   }
   public static double median(int[] arr){
-    int med = 0;
-    int medt = 0;
-    int med1 = 0;
-    int med3 = 0;
-    if (!(arr.length % 2 == 0)){ //if it is odd
-      med = (arr.length - 1) / 2;
-      return arr[med];
-
-    }
-    else{//if it is even
-      med1 = (arr.length / 2);
-      medt = (arr.length / 2) - 1;
-      med3 = (arr[med1] + arr[medt]) / 2;
-      return med3;
-
-    }
+      int n = arr.length;
+      double m=0;
+      if (n%2==1){
+        m = arr[(n+1)/2-1];
+	    }
+      else {
+        m = (arr[n/2-1] + arr[n/2])/2;
+      }
+       return m;
   }
 }
